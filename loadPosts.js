ad53ecf4b3b3973af20c161c1d9ac05f3704de2c
Acacia Mahon <acaciamahon@luxoradesignhouse.com>
2025-05-30 22:54:23 -0700

    Test blog post rendering

--- loadPosts.js
+++ loadPosts.js
@@ -1,20 +1,11 @@
-import { fetchJournalPosts } from './getPosts.js';
+import { fetchJournalPosts } from "./getPosts";
 
 async function renderPosts() {
-    const posts = await fetchJournalPosts();
-      const container = document.getElementById("journal-posts");
+  console.log("Blog script is working");
 
-        posts.forEach((post) => {
-        const div = document.createElement("div");
-        div.className = "blog-post";
-        div.innerHTML = '
-          <img src="${post.imageUrl}" alt="${post.title}" />
-          <h2>${post.title}</h2>
-          <p>${post.excerpt}</p>
-          <small>${new Date(post.publishedAt).toLocaleDateString()}</small>
-        ';
-        container.appendChild(div);
-  });
+  const container = document.getElementById("journal-posts");
+
+  container.innerHTML = "<p>This is a test blog post container.</p>";
 }
 
 renderPosts();
